6de0a93747bcad7351a58be996af7d36f7ef2b0e
ArHasan <arhasan.cse@gmail.com>
2020-08-23 11:22:48 +0600

    class-1

---
+++ varivale-1.2.php
@@ -0,0 +1,25 @@
+<?php
+// $name = 'ArHasan';
+// $age = 24;
+// $question = "How are your?";
+
+// echo 'Hello' . $name;
+// echo "\n";
+
+// echo "Hi $name";
+// echo 'HI $name'; //singel coute not execute variable
+
+// echo "Hello {$name},{$question}\n";
+
+//class-1.3
+define('PI',3.14169);
+$task = 'Read';
+$task = 'Write';
+echo $task;
+echo "\n";
+echo "Value of PI = {PI}".PI;
+echo "\n";
+// echo constant('PI');
+
+$constant = 'constant';
+echo "Value of PI = {$constant('PI')}";
