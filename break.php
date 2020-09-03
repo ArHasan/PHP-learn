8b192d46e5654ea884c2970bd4bdd4896a613827
ArHasan <arhasan.cse@gmail.com>
2020-09-03 10:57:13 +0600

    3-sep

---
+++ break.php
@@ -0,0 +1,19 @@
+<?php
+for($i=1; $i<=50; $i++){
+   
+    //echo "$i\n"; 
+    // if($i==2){
+    // break;}
+
+    // if($i % 13==0){
+    //     echo $i;
+    //     echo PHP_EOL;
+    // break;}
+
+        if($i<45){
+            goto a;
+        
+        }
+        echo "$i\n";
+        a: 'y';
+}
---
+++ fibonacci.php
@@ -0,0 +1,11 @@
+<?php
+$n = 10;
+$firstNum= 0;
+$secondNum = 1;
+$sum = 0;
+for($i=0; $i< $n; $i++){
+    $sum = $a+$sum;
+    echo $sum."\n";
+    $a=$b+$sum;
+    $b=$sum;
+}
