54ad41e248349f82b171c16f89cc9aa67462890e
ArHasan <arhasan.cse@gmail.com>
2021-04-20 02:09:26 +0600

    add_new 🙄

--- problemSolving/test.php
+++ problemSolving/test.php
@@ -1,5 +1,8 @@
 <?php 
 
-printf("The binary of %d is %b \n",7,7);
-printf("The hexadecimal of %d is %X \n",2500,2500);
-printf("The octal of %d is %o \n",25,25);
+$d  = 'A00';
+
+for($i=0;$i<5;$i++) {
+    echo ++$d;
+    echo PHP_EOL;
+}
