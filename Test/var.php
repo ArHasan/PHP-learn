f030907d2ba231328fedc28f0f21eb0ad57ef572
ArHasan <arhasan.cse@gmail.com>
2022-04-26 23:28:38 +0600

    testAdd

--- Test/var.php
+++ Test/var.php
@@ -4,4 +4,21 @@ $greeting = 'Hello ';
 
 $name = 'Hasan';
 
-echo  "{$greeting}{$name}";
+// echo  "{$greeting}{$name}";
+
+$num = [2,7,11,15];
+$target = 9;
+$sum =0;
+$result = [];
+foreach($num as $key=>$item){
+    $sum = $item + $sum;
+    if($sum == $target){
+        array_push($result,$key);
+        echo $sum,"[$key]";
+        echo $sum;
+    }
+    echo $sum,"[$key]";
+    echo PHP_EOL;
+}
+echo $sum;
+print_r($result);
